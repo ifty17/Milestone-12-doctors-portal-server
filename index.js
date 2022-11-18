63de49b4f969ac45f0da34a72a8598454aeec432
ifty17 <iftyrahatkabir@gmail.com>
2022-11-18 11:19:04 +0600

    added admin role

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
@@ -23,6 +24,25 @@ async function run(){
     try{
         const appointmentOptionCollection = client.db('doctorsPortal').collection('appointmentOptions');
         const bookingsCollection = client.db('doctorsPortal').collection('bookings');
+        const usersCollection = client.db('doctorsPortal').collection('users');
+
+        function verifyJWT(req, res, next){
+            const authHeader = req.headers.authorization;
+            if(!authHeader){
+                 return res.status(401).send('unauthorized access');
+            }
+            const token = authHeader.split(' ')[1];
+
+            jwt.verify(token, process.env.ACCESS_TOKEN, function(err, decoded){
+                if(err){
+                    return res.status(403).send({message: 'forbidden access'})
+                }
+                req.decoded = decoded;
+                next();
+            })
+
+        }
+
         // Use Aggregate to query multiple collection and then merge data
         app.get('/appointmentOptions', async(req, res) =>{
             const date = req.query.date;
@@ -92,14 +112,27 @@ async function run(){
         })
 
 
-         app.get('/bookings', async(req, res) =>{
+         app.get('/bookings', verifyJWT, async(req, res) =>{
             const email = req.query.email;
-            console.log(email);
+            const decodedEmail = req.decoded.email;
+
+            if(email !== decodedEmail){
+                return res.status(403).send({message: 'forbidden access'})
+            }
+
             const query = {email: email};
             const bookings = await bookingsCollection.find(query).toArray();
             res.send(bookings);
          })
          
+
+
+         app.get('/users', async(req, res) =>{
+            const query = {};
+            const users = await usersCollection.find(query).toArray();
+            res.send(users);
+         })
+
          app.post('/bookings', async (req, res) => {
             const booking = req.body;
             console.log(booking);
@@ -120,6 +153,38 @@ async function run(){
             res.send(result);
         })
 
+        app.get('/jwt', async(req, res) =>{
+            const email = req.query.email;
+            const query = {email: email}
+            const user = await usersCollection.findOne(query);
+            console.log(user);
+            if(user){
+                const token = jwt.sign({email}, process.env.ACCESS_TOKEN, {expiresIn: '1h'})
+                return res.send({accessToken: token});
+            }
+            res.status(403).send({accessToken: ''})
+        })
+
+        app.post('/users', async(req, res) =>{
+            const user = req.body;
+            const result = await usersCollection.insertOne(user);
+            res.send(result);
+
+        })
+
+        app.put('/users/admin/:id', async(req, res) =>{
+            const id = req.params.id;
+            const filter = {_id: ObjectId(id)};
+            const options = {upsert: true};
+            const updateDoc = {
+                $set:{
+                    role: 'admin'
+                }
+            }
+            const result = await usersCollection.updateOne(filter, options, updateDoc);
+            res.send(result);
+
+        })
 
     }
     finally{
